ae57410f94d982c731c852b0130d0e7e7fda6b3a
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2023-11-16 17:57:58 +0900

    feat : 여행 계획 리디자인

---
+++ enjoytripvue/src/components/map/MapSelect.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import OptionItem from '@/components/map/item/OptionItem.vue'
+    import MapItem from './item/MapItem.vue';
+    import SearchList from './SearchList.vue';
+    import {ref, watch} from 'vue';
+    const option = ref([]);
+    const attInfo = ref([]);
+
+    function confirmOption(data){
+        option.value = data;
+    }
+
+    function confirmList(data){
+        attInfo.value = data;
+    }
+    watch(option,()=>{
+        console.log("MapSelect......................option", option.value);
+    },
+    {deep:true});
+
+    watch(attInfo,()=>{
+        console.log("MapSelect......................attInfo", option.value);
+    },
+    {deep:true});
+
+</script>
+<template>
+    <div class="mapWrapper">
+        <MapItem :options="option" @getAttList="confirmList"></MapItem>
+        <OptionItem @changeOption="confirmOption"></OptionItem>
+        <SearchList :attInfo="attInfo" ></SearchList>
+    </div>
+</template>
+<style>
+.mapWrapper{
+    position: relative;
+    display: flex;
+}
+</style>
--- enjoytripvue/src/router/index.js
+++ enjoytripvue/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TheMainView from '../views/TheMainView.vue';
 import TheSignUpView from '@/views/TheSignUpView.vue';
-import TheMapView from '@/views/TheMapView.vue';
+import TheMapView from '@/components/map/MapSelect.vue';
 // import TheBoardView from "../views/TheBoardView.vue";
 
 const router = createRouter({
